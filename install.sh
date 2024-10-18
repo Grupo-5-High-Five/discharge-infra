5523dd46d33173e853c226119800160d8b047b10
joaoGlima09 <joao.glima@sptech.school>
2024-10-18 19:50:04 -0300

    feat: :art: script de instalação inicial

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt update && sudo apt upgrade -y
+
+mkdir infra
+cd infra/
+
+git clone https://github.com/Grupo-5-High-Five/discharge-infra.git
+
+cd discharge-infra/
+
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+
+sudo docker-compose up -d
+
+# sudo docker-compose down -v && sudo docker-compose down --rmi all -v
+
+
