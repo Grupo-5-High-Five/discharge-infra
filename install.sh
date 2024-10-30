b2e4acdd831383f3d51e39e92d0eb7a7f1ff2f52
GustavoLuzz <161068517+GustavoLuzz@users.noreply.github.com>
2024-10-30 11:52:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY="" 
 AWS_SESSION_TOKEN=""
-
 sudo apt update && sudo apt upgrade -y
 
 mkdir -p app
@@ -19,22 +18,27 @@ AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
 EOF
 
+# Instale o Docker e o Docker Compose
 sudo apt install docker.io -y
+sudo apt install unzip curl -y  
 sudo apt install docker-compose -y
-sudo apt install unzip curl -y 
 
+# Instale o AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
+# Inicie os containers com o Docker Compose
 sudo docker-compose up -d
 
+# Liste os containers em execução
 sudo docker ps -a
 
-CRON="* * * * * docker start java_container && docker ps -a >> /tmp/cron.log 2>&1 && docker logs java_container > /tmp/logs.txt && /usr/local/bin/aws s3 cp /tmp/logs.txt s3://discharge-bucket/log_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log && rm /tmp/logs.txt >> /tmp/cron.log 2>&1"
+# Configure o cron para rodar o container e enviar logs para o S3
+CRON="* * * * * sudo docker start java_container && sudo docker logs java_container > /tmp/logs.txt && aws s3 cp /tmp/logs.txt s3://discharge-bucket/log_$(date +'%Y-%m-%d_%H-%M-%S').log && rm /tmp/logs.txt"
 
-if crontab -l | grep -Fxq "$CRON"
-then
+# Adicione a entrada ao crontab, se não existir
+if crontab -l | grep -Fxq "$CRON"; then
     echo "Crontab já existe."
 else
     (crontab -l; echo "$CRON") | crontab -
